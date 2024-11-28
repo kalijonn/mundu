62faab4141389026702d5c1f51e6e2b123407f6a
kalijonn <43421621+kalijonn@users.noreply.github.com>
2024-11-28 23:36:05 +0530

    add disabled prop, size variants

--- src/components/primitives/Button/Button.tsx
+++ src/components/primitives/Button/Button.tsx
@@ -4,10 +4,14 @@ import styles from "./button.module.css";
 
 export type ButtonProps = ComponentProps<"button"> & {
   variant: "primary" | "secondary" | "destructive";
+  size?: "small" | "medium" | "large";
 };
 
-export const Button = ({ variant, ...props }: ButtonProps) => {
+export const Button = ({ variant, size = "medium", ...props }: ButtonProps) => {
   return (
-    <button {...props} className={`${styles.default} ${styles[variant]}`} />
+    <button
+      className={`${styles.default} ${styles[variant]} ${styles[size]}`}
+      {...props}
+    />
   );
 };
